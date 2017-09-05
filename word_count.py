afc22477474cac3c3a19896a1e389ea1ce11935e
Pablo Barreyro <Pabarreyro@gmail.com>
2017-09-05 10:03:46 -0700

    First pass of word count exercise

--- word_count.py
+++ word_count.py
@@ -18,17 +18,11 @@
 #     print(contents)
 #
 
-
 import string
-
-
-def punct_scrub(file_path):
-    file = open(file_path, 'w')
-    file.translate(string.maketrans("", ""), string.punctuation)
+import operator
 
 def word_count(file_path):
-    file = open(file_path, 'r')
-    count = {}
+    ct_dict= {}
     stop_words = ['a', 'the', 'is', 'i', 'an', 'is', 'me', 'my', 'he', 'his', 'her',
                   'they', 'them', 'their', 'we', 'our', 'us', 'to', 'from', 'and',
                   'but', 'was', 'be', 'in', 'on', 'have', 'it', 'that', 'she', 'with', 'do',
@@ -36,20 +30,35 @@ def word_count(file_path):
                   'would', 'all', 'make', 'when', 'which', 'some', 'into', 'see', 'your', 'now',
                   'than', 'like', 'how', 'these', 'want', 'way', 'look', 'first', 'also', 'new',
                   'day', 'more', 'use', 'no', 'yes', 'man', 'find', 'here', 'thing', 'give', 'many',
-                  'well', 'one', 'very', 'by']
-    words = file.read().lower().split()
-    for word in words:
-        if word not in stop_words:
-                if word in count:
-                    count[word] += 1
-                else:
-                    count[word] = 1
-    print(count)
-    lst_count = (word, count[word])
-    print(lst_count)
+                  'well', 'one', 'very', 'by', 'of', 'had', 'him', 'at', 'for', 'room', 'there', 'been',
+                  'so', 'back', 'even', 'did', 'were', 'are', 'about', 'only', 'just', 'said', 'any', 'other',
+                  'could', 'other', 'then', 'up', 'you', 'out', 'still', 'made', 'while', 'mother', 'father',
+                  'sister', 'brother', 'again', 'with', 'without', 'much', 'after', 'before', 'where', 'down',
+                  'open', 'close', 'it', 'its', 'himself', 'herself', 'time', 'went', 'come', 'away', 'soon',
+                  'family', 'came', 'left', 'right', 'too', 'though', 'look', 'hear', 'everything', 'little', 'big',
+                  'thought', 'over', 'quite', 'looked', 'wanted', 'something', 'let', 'two', 'against', 'seemed',
+                  'being', 'work', ]
+    file = open(file_path, 'r')
+    for line in file:
+        scrub_list = line.replace(',', '').replace('(', '').replace(')', '').replace('\'', '').replace('.', '').replace(
+            ';', '').replace('!', '').replace('"', '').replace('?', '').replace(':', '').replace('-', '').lower().split()
+        for word in scrub_list:
+            if word not in stop_words:
+                    if word in ct_dict:
+                        ct_dict[word] += 1
+                    else:
+                        ct_dict[word] = 1
+    sort_ct_dict = sorted(ct_dict.items(), key=operator.itemgetter(1),  reverse=True)
+    print("Here are the ten most common * unique * words in your book:\n")
+    print("1.\t'{}', {} times".format(sort_ct_dict[0][0], sort_ct_dict[0][1]))
+    print("2.\t'{}', {} times".format(sort_ct_dict[1][0], sort_ct_dict[1][1]))
+    print("3.\t'{}', {} times".format(sort_ct_dict[2][0], sort_ct_dict[2][1]))
+    print("4.\t'{}', {} times".format(sort_ct_dict[3][0], sort_ct_dict[3][1]))
+    print("5.\t'{}', {} times".format(sort_ct_dict[4][0], sort_ct_dict[4][1]))
+    print("6.\t'{}', {} times".format(sort_ct_dict[5][0], sort_ct_dict[5][1]))
+    print("7.\t'{}', {} times".format(sort_ct_dict[6][0], sort_ct_dict[6][1]))
+    print("8.\t'{}', {} times".format(sort_ct_dict[7][0], sort_ct_dict[7][1]))
+    print("9.\t'{}', {} times".format(sort_ct_dict[8][0], sort_ct_dict[8][1]))
+    print("10.\t'{}', {} times".format(sort_ct_dict[9][0], sort_ct_dict[9][1]))
 
-
-punct_scrub('metamorphosis.txt')
-
-
-# word_count('metamorphosis.txt')
+word_count('kafka.txt')
