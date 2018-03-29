7b227c35b796cf2d69ae2d57cb982b95567d17cc
Pablo Barreyro <Pabarreyro@gmail.com>
2018-03-29 10:24:57 -0700

    add business logic for converting hundreds digit

--- exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
+++ exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
@@ -2,6 +2,7 @@
 
 var onesNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 var tensNumerals = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+var hundredsNumerals = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 
 function convertOnes(number) {
   return onesNumerals[number-1];
@@ -12,3 +13,9 @@ function convertTens(number) {
   var tenDigit = parseInt(numberString[0]);
   return tensNumerals[tenDigit-1];
 };
+
+function convertHundreds(number) {
+  var numberString = number.toString();
+  var hundredDigit = parseInt(numberString[0]);
+  return hundredsNumerals[hundredDigit-1];
+}
