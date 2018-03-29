5758ca52d44416e1df7238ad1145ef99735b7765
Pablo Barreyro <Pabarreyro@gmail.com>
2018-03-29 12:27:05 -0700

    add business logic for converting thousands digit

--- exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
+++ exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
@@ -3,6 +3,7 @@
 var onesNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 var tensNumerals = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 var hundredsNumerals = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var thousandsNumerals = ["M", "MM", "MMM"];
 
 function convertOnes(number) {
   return onesNumerals[number-1];
@@ -18,4 +19,10 @@ function convertHundreds(number) {
   var numberString = number.toString();
   var hundredDigit = parseInt(numberString[0]);
   return hundredsNumerals[hundredDigit-1];
+}
+
+function convertThousands(number) {
+  var numberString = number.toString();
+  var thousandDigit = parseInt(numberString[0]);
+  return thousandsNumerals[thousandDigit-1];
 }
