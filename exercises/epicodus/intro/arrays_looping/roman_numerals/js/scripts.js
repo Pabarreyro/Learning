6f80d1fb2ebdb59a3781f56c8d1b52959d5b7c34
Pablo Barreyro <Pabarreyro@gmail.com>
2018-03-29 09:21:51 -0700

    add business logic for converting tens digit

--- exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
+++ exercises/epicodus/intro/arrays_looping/roman_numerals/js/scripts.js
@@ -1,7 +1,14 @@
 // Backend logic
 
-var onesNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+var onesNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var tensNumerals = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 
 function convertOnes(number) {
   return onesNumerals[number-1];
+};
+
+function convertTens(number) {
+  var numberString = number.toString();
+  var tenDigit = parseInt(numberString[0]);
+  return tensNumerals[tenDigit-1];
 };
