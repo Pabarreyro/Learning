924a7b550288dd5186993fcc3a58a200ea555331
Guest User <Guest@Epicodus-9C.local>
2018-04-17 12:13:30 -0700

    add node project template directory

---
+++ exercises/epicodus/js/node_template/src/sample_logic.js
@@ -0,0 +1,14 @@
+export class Type {
+  constructor(arg1, arg2, arg3) {
+    this.arg1 = arg1;
+    this.arg2 = arg2;
+    this.arg3 = arg3;
+  }
+
+  someMethod() {
+  }
+};
+
+class otherType extends Type {
+
+}
