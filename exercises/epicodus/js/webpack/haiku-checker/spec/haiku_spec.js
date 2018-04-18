16e4bf8b53d05dabc18ec4ea64f832fc24513c2e
Pablo Barreyro <pabarreyro@gmail.com>
2018-04-18 16:50:33 -0700

    update specs and logic files

--- exercises/epicodus/js/webpack/haiku-checker/spec/haiku_spec.js
+++ exercises/epicodus/js/webpack/haiku-checker/spec/haiku_spec.js
@@ -2,6 +2,7 @@ import { checkLines } from './../src/project.js';
 import { splitSentences } from './../src/project.js';
 import { countVowels } from './../src/project.js';
 import { countWordsEndingE } from './../src/project.js';
+import { updateVowelCount } from './../src/project.js';
 
 describe('Haiku', function() {
   let reusableText = "Haikus are easy.\nBut sometimes they don't make sense.\nRefrigerator.";
@@ -14,12 +15,15 @@ describe('Haiku', function() {
     expect(splitSentences(reusableText).length).toEqual(3);
   });
 
-  it('should count the number of vowels or vowel pairs in each sentence', function() {
-    expect(countVowels(reusableText)).toEqual([5,11,5]);
+  it('should count the number of vowels, vowel pairs, and qualifying ys in each sentence', function() {
+    expect(countVowels(reusableText)).toEqual([6,11,5]);
   });
 
-  it('should count the number of instances that a word ends with e in each sentence', function() {
-    expect(countWordsEndingE(reusableText)).toEqual([1,1,0]);
+  it('should count the number of instances that a word ends with ie, e, but not le, in each sentence', function() {
+    expect(countWordsEndingE(reusableText)).toEqual([1,2,0]);
   });
 
+  it('should subtract number of words that end in e from the number of vowels in the sentence', function() {
+    expect(updateVowelCount(reusableText)).toEqual([5,9,5]);
+  });
 });
--- exercises/epicodus/js/webpack/haiku-checker/src/project.js
+++ exercises/epicodus/js/webpack/haiku-checker/src/project.js
@@ -13,12 +13,34 @@ export function countVowels(text) {
   let countArray = [];
 
   for (let i = 0; i < sentenceArray.length; i++) {
-    countArray.push(sentenceArray[i].match(/[aeiou]+/gi).length);
+    countArray.push(sentenceArray[i].match(/[aeu]+|i|[cts]ia+|e[^o]\W|[ou]+/gi).length);
   }
   return countArray;
 }
 
 
 export function countWordsEndingE(text){
-  console.log(text);
+  let sentenceArray = splitSentences(text);
+  let countArray = [];
+
+  for (let i = 0; i < sentenceArray.length; i++) {
+    if (sentenceArray[i].match(/[a-z]+e\s/gi) !== null) {
+      countArray.push(sentenceArray[i].match(/ie|[a-z]+[^lh]e\W|\s[^rh]ele/gi).length);
+    } else {
+      countArray.push(0);
+    }
+  }
+  return countArray;
+}
+
+export function updateVowelCount(text) {
+  let sentenceArray = splitSentences(text);
+  let vowelCount = countVowels(text);
+  let endineCount = countWordsEndingE(text);
+  let updatedVowels = [];
+
+  for (var i = 0; i < sentenceArray.length; i++) {
+    updatedVowels.push(vowelCount[i] - endineCount[i]);
+  }
+  return updatedVowels;
 }
