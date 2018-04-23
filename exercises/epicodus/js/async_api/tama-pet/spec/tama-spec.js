883d2223fda9c094110a3cb0108544341f3fb7b3
Pablo Barreyro <pabarreyro@gmail.com>
2018-04-23 11:50:07 -0700

    add tamagotchi practice project with initial backend logic

---
+++ exercises/epicodus/js/async_api/tama-pet/spec/tama-spec.js
@@ -0,0 +1,42 @@
+import { pet } from './../src/tama-pet.js';
+
+describe('TamaPet', function() {
+  let testPet = pet;
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    testPet.hunger();
+    testPet.pooper();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+    testPet.hunger();
+    testPet.pooper();
+  });
+
+  it('should create an initial pet', function() {
+    expect(testPet.tummy).toEqual(10);
+  });
+  it('should die when tummy is empty', function() {
+    jasmine.clock().tick(10001);
+    expect(testPet.tummy).toEqual(0);
+  });
+  it('should after 10 seconds if not fed', function() {
+    jasmine.clock().tick(10001);
+    expect(testPet.petCemetery()).toEqual(true);
+  });
+  it('should get dirty every 20 seconds', function() {
+    jasmine.clock().tick(20001);
+    expect(testPet.dirty).toEqual(1);
+  });
+  it('should want to play only when it fed', function() {
+    jasmine.clock().tick(7001);
+    expect(testPet.play()).toEqual(false);
+  });
+  it('should sleep when hours reach between 19 and 9', function() {
+    testPet.date.setHours(20);
+    testPet.sleep();
+    expect(testPet.sleep).toEqual(true);
+  });
+});
---
+++ exercises/epicodus/js/async_api/tama-pet/src/tama-pet.js
@@ -0,0 +1,45 @@
+let pet = {
+  date: new Date(),
+  tummy: 10,
+  dirty: 0,
+  sleep: false,
+  hunger: function(){
+    const hungerInterval = setInterval(()=> {
+      this.tummy--;
+      if (this.tummy == 0) {
+        clearInterval(hungerInterval);
+        return "Why did you kill me daddy?";
+      }
+    }, 1000)
+  },
+  petCemetery: function(){
+    if (this.tummy > 0 && this.dirty < 10) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  play: function(){
+    if (this.tummy > 5) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  sleep: function() {
+    if(this.date.getHours() > 19 && this.date.getHours() < 9) {
+      this.sleep = true;
+    }
+  },
+  pooper: function(){
+    const poopInterval = setInterval(()=> {
+      this.dirty++;
+      if (this.dirty > 5) {
+        clearInterval(poopInterval);
+        return "Why did you kill me daddy?";
+      }
+    }, 20000)
+  }
+};
+
+export { pet }
