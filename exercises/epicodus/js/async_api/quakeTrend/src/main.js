1a6db5c4f237ec8a869bbc79de555a5a0842cb54
Pablo Barreyro <pabarreyro@gmail.com>
2018-04-26 16:52:25 -0700

    refactor quakes project; add jobs-finder project

--- exercises/epicodus/js/async_api/quakeTrend/src/main.js
+++ exercises/epicodus/js/async_api/quakeTrend/src/main.js
@@ -6,33 +6,50 @@ import './styles.css';
 const mapsapi = require( 'google-maps-api' )( process.env.API_KEY );
 
 let map;
-function initMap(response) {
-  mapsapi().then(function(maps) {
-    map = new maps.Map(document.getElementById('map'), {
+let maps;
+let markers = [];
+function initMap() {
+  mapsapi().then(function(mapsObj) {
+    maps = mapsObj;
+    map = new mapsObj.Map(document.getElementById('map'), {
       zoom: 2,
       center: {lat: 0, lng: 0}
     });
-    for (var i = 0; i < response.features.length; i++) {
-      let magnitude = response.features[i].properties.mag;
-      let coords = response.features[i].geometry.coordinates;
-      let latLng = new maps.LatLng(coords[1],coords[0]);
-      let marker = new maps.Marker({
-        position: latLng,
-        map: map,
-        icon: getCircle(magnitude)
-      });
-    }
-    function getCircle(magnitude) {
-      return {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: .2,
-        scale: Math.pow(2, magnitude) / 2,
-        strokeColor: 'white',
-        strokeWeight: .5
-      };
-    }
   });
+}
+
+function drawCircles(response){
+  for (var i = 0; i < response.features.length; i++) {
+    let magnitude = response.features[i].properties.mag;
+    let coords = response.features[i].geometry.coordinates;
+    let latLng = new maps.LatLng(coords[1],coords[0]);
+    let marker = new maps.Marker({
+      position: latLng,
+      map: map,
+      icon: getCircle(magnitude)
+    });
+    markers.push(marker);
+  }
+  function getCircle(magnitude) {
+    return {
+      path: maps.SymbolPath.CIRCLE,
+      fillColor: 'red',
+      fillOpacity: .2,
+      scale: Math.pow(2, magnitude) / 2,
+      strokeColor: 'white',
+      strokeWeight: .5
+    };
+  }
+}
+
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function clearMarkers() {
+  setMapOnAll(null);
 }
 
 $(function() {
@@ -61,8 +78,9 @@ $(function() {
       $('#date').text(quakeDays[0]);
       quakePromise = api.quakesCall(quakeDays[0],quakeDays[1]);
       quakeDays.shift();
+      clearMarkers();
       quakePromise.then(function(response) {
-        initMap(response);
+        drawCircles(response);
       }, function(error) {
         console.log(`${error.message}`);
       });
