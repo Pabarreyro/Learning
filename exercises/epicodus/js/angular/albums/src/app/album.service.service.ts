dda45286594ea9feb3321823505bad3d0ee2f143
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-22 19:39:52 -0700

    Commit changes before merging

--- exercises/epicodus/js/angular/albums/src/app/album.service.service.ts
+++ exercises/epicodus/js/angular/albums/src/app/album.service.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Album.ServiceService {
-
-  constructor() { }
-
-}
+// import { Injectable } from '@angular/core';
+//
+// @Injectable()
+// export class Album.ServiceService {
+//
+//   constructor() { }
+//
+// }
--- exercises/epicodus/js/angular/albums/src/app/marketplace/marketplace.component.ts
+++ exercises/epicodus/js/angular/albums/src/app/marketplace/marketplace.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
+import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 
-export class MarketplaceComponent {
+export class MarketplaceComponent implements OnInit {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private albumService: AlbumService){}
 
-  albums: Album[];
-
-  goToDetailPage(clickedAlbum: Album) {
-    this.router.navigate(['albums', clickedAlbum.id]);
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
   }
+
+  goToDetailPage(clickedAlbum) {
+    this.router.navigate(['albums', clickedAlbum.$key]);
+  };
 }
