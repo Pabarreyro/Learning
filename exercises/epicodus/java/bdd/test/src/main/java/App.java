1093b55400b1f8e88997cbfa6bb3980b83ef4dae
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 09:50:45 -0700

    Create test files for testing Intellij plugins

---
+++ exercises/epicodus/java/bdd/test/src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        public String foo = "hello";
+        public String world = "world";
+
+
+    }
+}
---
+++ exercises/epicodus/java/bdd/test/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void newGame_instantiates() {
+
+    }
+
+    @Test
+    public void new() {
+    }
+}
