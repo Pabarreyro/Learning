b5e29136bb530b0a311a37bae09e5899c9edc870
Pablo Barreyro <Pabarreyro@gmail.com>
2020-01-19 12:11:48 -0800

    Non-unique exercise

--- checkio/check_io.js
+++ checkio/check_io.js
@@ -1,4 +1,36 @@
 /*
+Input: A list of integers.
+
+Output: An iterable of integers.
+
+Precondition: 0 < len(data) < 1000
+ */
+function nonUniqueElements(data) {
+    var filterData = [];
+    for (var i=0; i<data.length; i++) {
+        for (var j=data.length-1; j>=0; j--) {
+            if (i !== j && data[i] === data[j]) {
+                filterData.push(data[i]);
+                break;
+            }
+        }
+    }
+
+    return filterData;
+}
+/*
+*****************
+* index methods *
+*****************
+ function filterNonUnique(data) {
+    return data.filter(function(a){
+        return data.indexOf(a) !== data.lastIndexOf(a)    
+    });
+}
+*/
+
+
+/*
 Input: An array as a list of integers.
 
 Output: The median as a float or an integer.
@@ -19,10 +51,23 @@ function median(data) {
     }
 }
 
-/*
+/* 
+****************
+* floor + ceil *
+****************
 function median(data) {
     let sorted = data.slice().sort((a,b) => a - b);
     let middle = (data.length - 1) / 2;
     return (sorted[Math.floor(middle)] + sorted[Math.ceil(middle)])/2;
+}
+
+************
+* manually *
+************
+function median(data) {
+    let len = data.length;
+    let sorted = data.slice().sort((a,b) => a - b);
+    let mid = Math.floor(len / 2);
+    return (sorted[mid] + sorted[(len - 1 - mid)]) / 2;
 }
 */
