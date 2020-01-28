a5423a1950a5c64be0ca12932c52c3c17f7ec976
Pablo Barreyro <Pabarreyro@gmail.com>
2020-01-27 21:54:27 -0800

    Add click event to menu button

--- exercises/html_css_js/html_css/jam_n_java/js/main.js
+++ exercises/html_css_js/html_css/jam_n_java/js/main.js
@@ -4,3 +4,9 @@
 window.addEventListener('load', () => {
   document.querySelector('.preloader').style.display = "none";
 });
+
+// nav btn
+document.querySelector('.nav_btn').addEventListener('click', () => {
+  document.querySelector('.nav_menu').classList.toggle('nav_menu-display');
+  document.querySelector('.nav_btn').classList.toggle('nav_btn-close').toggle('');
+})
